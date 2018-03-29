dffaf0d2b9c2066cd8347308893bd0f9f029fc27
Gema <Gema@192.168.0.13>
2018-03-29 18:06:06 +0200

    First commit

---
+++ project-2018/src/main/java/com/example/project2018/Project2018Application.java
@@ -0,0 +1,13 @@
+package com.example.project2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2018Application.class, args);
+		//test
+	}
+}
