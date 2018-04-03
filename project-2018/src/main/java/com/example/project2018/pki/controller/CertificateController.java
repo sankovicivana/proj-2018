e188a926a652302176d4633f665803d4e226f1cc
stefan <stefan@stefan-Inspiron-3521>
2018-04-03 09:48:22 +0200

    Odradjen controller za validaciju.

--- project-2018/src/main/java/com/example/project2018/pki/controller/CertificateController.java
+++ project-2018/src/main/java/com/example/project2018/pki/controller/CertificateController.java
@@ -3,6 +3,8 @@ package com.example.project2018.pki.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -83,5 +85,13 @@ public class CertificateController {
 		
 	return new ResponseEntity<>(cert, HttpStatus.OK);
 	}
+	
+	
+	@GetMapping("/valid/{id}")
+	public ResponseEntity<?> isValid(@PathVariable String id){
+			service.isValid(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	 
 	
 }
