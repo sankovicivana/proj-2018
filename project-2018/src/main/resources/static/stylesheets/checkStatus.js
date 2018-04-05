9ab617ecd0f0165d9bf2241213abbf77d3c30fc9
Igor Zeljkovic <zeljkovic18@gmail.com>
2018-04-05 05:13:35 +0200

    odradjen front za proveravanje statusa sertifikata.

--- project-2018/src/main/resources/static/stylesheets/checkStatus.js
+++ project-2018/src/main/resources/static/stylesheets/checkStatus.js
@@ -3,15 +3,29 @@ $(document).ready(function(){
 	$('#checkBut').click(function(){
 		var url = "certificate/" + $("#searchField").val();
 		console.log(url);
+		
+		$("#inputCheck").modal("toggle");
+		
+		$("#checkDiv").empty();
+		$("#exampleModalLabelReg2").empty();
 	
 		$.ajax({
 			url: url,
 			success: function(data){
 				
-				console.log(data);
+				$("#exampleModalLabelReg2").append("Certificate with serial number: " + data.serialNumber);
+				
+				newCertificate = "<div><label>Serial number:</label>"  + data.serialNumber + "</div>"
+								+"<div><label>Issuer:</label>"  + data.issuer + "</div>"
+								+"<div><label>Valid from:</label>"  + data.validFrom + "<strong> to </strong>" + data.validUntil + "</div>"
+								+"<div><label>AIA:</label>"  + data.aia + "</div>";
+								
+				$("#checkDiv").empty().append(newCertificate);
 				
 			}
 		});
+		
+		$("#checkCertificate").modal("toggle");
 	});
 	
 	
