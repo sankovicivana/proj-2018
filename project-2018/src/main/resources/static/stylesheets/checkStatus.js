d7769ab5efd7681af39a06dc4745df34257487d4
Gema <gemovics@gmail.com>
2018-05-21 12:15:19 +0200

    check status

--- project-2018/src/main/resources/static/stylesheets/checkStatus.js
+++ project-2018/src/main/resources/static/stylesheets/checkStatus.js
@@ -1,5 +1,33 @@
-var url="certificate/checkStatus";
-
-$(document).on('click','.searchForm',function(event){
-	console.log('Testing console');
+$(document).ready(function(){
+	
+	$('#checkBut').click(function(){
+		var url = "certificate/" + $("#searchField").val();
+		console.log(url);
+		
+		$("#inputCheck").modal("toggle");
+		
+		$("#checkDiv").empty();
+		$("#exampleModalLabelReg2").empty();
+	
+		$.ajax({
+			url: url,
+			success: function(data){
+				
+				$("#exampleModalLabelReg2").append("Certificate with serial number: " + data.serialNumber);
+				
+				newCertificate = "<div><label>Serial number:</label>"  + data.serialNumber + "</div>"
+								+"<div><label>Issuer:</label>"  + data.issuer + "</div>"
+								+"<div><label>Valid from:</label>"  + data.validFrom + "<strong> to </strong>" + data.validUntil + "</div>"
+								+"<div><label>AIA:</label>"  + data.aia + "</div>";
+								
+				$("#checkDiv").empty().append(newCertificate);
+				
+			}
+		});
+		
+		$("#checkCertificate").modal("toggle");
+	});
+	
+	
+	 
 });
