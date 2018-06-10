18c47a0390223d3328ed4a13596f94446cd19174
Igor Zeljkovic <zeljkovic18@gmail.com>
2018-06-10 22:39:47 +0200

    modeli

---
+++ project-2018/src/main/java/com/example/project2018/server/model/Price.java
@@ -0,0 +1,63 @@
+package com.example.project2018.server.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Price {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private int price;
+	
+	@Column
+	private Date startingDate;
+	
+	@Column
+	private Date endingDate;
+
+	public Price() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public Date getStartingDate() {
+		return startingDate;
+	}
+
+	public void setStartingDate(Date startingDate) {
+		this.startingDate = startingDate;
+	}
+
+	public Date getEndingDate() {
+		return endingDate;
+	}
+
+	public void setEndingDate(Date endingDate) {
+		this.endingDate = endingDate;
+	}
+	
+}
