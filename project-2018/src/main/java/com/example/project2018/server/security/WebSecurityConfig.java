51ebfd90ec6f326d4150041868c63e12970d8227
Milovic <Milovic@DESKTOP-BHBIRQC>
2018-06-12 13:34:27 +0200

    Dodata podesavanja za prelazak sa http-a na https.

--- project-2018/src/main/java/com/example/project2018/server/security/WebSecurityConfig.java
+++ project-2018/src/main/java/com/example/project2018/server/security/WebSecurityConfig.java
@@ -1,12 +1,12 @@
 package com.example.project2018.server.security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -106,4 +106,5 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
           
     }
 
+ 
 }
